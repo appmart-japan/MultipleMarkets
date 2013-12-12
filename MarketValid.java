804ee3f8adcbe50986cf487281606f5d334a7a2e
info-appmart <facebook@app-mart.jp>
2013-12-12 11:40:07 +0900

    Add comments on main class

--- MarketValid.java
+++ MarketValid.java
@@ -1,15 +1,26 @@
-//Package manager
+/**
+*　このサンプールコードを使うことによって
+*　アンドロイドアプリの提供元を判断することはできます。
+* 1- Google Market(c) からのインストールの場合は　Google Wallet(c) APIを実装
+* 2- その他のマーケットの場合はAppmartの課金APIを実装
+*
+*　Google Marketの課金APIはこちら
+* http://developer.android.com/google/play/billing/billing_integrate.html
+*
+* Appmartの課金APIはこちら
+* https://github.com/info-appmart/inBillingSampleOnePage
+**/
+
+//Package manager取得
 PackageManager pm = getPackageManager();
 String installationSource = pm.getInstallerPackageName(getPackageName());
- 
-String source ;
- 
+  
 //提供元を判断
 if(installationSource.equals("com.android.vending")){
- source= "google play";	//google
-}else if(installationSource.equals("com.amazon.venezia")){
- source= "amazon market";//amazon
+	Log.d("DEBUG", "google Market(c)からインストールされました。");
+	//TODO google WalletのAPIを実装
 }else{
- source= "その他のマーケット";//その他
+	Log.d("DEBUG", "google Market(c)意外のマーケットからインストールされました。");
+	//TODO Appmartの課金APIを実装
 }
- Log.d("DEBUG", source + "からインストールされました。");
+
